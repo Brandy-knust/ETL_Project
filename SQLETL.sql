fa57e11525a8dc3b239a005b9c433f94b782ecf1
Brandy Knust <brandy_1175@hotmail.com>
2021-04-16 17:47:10 -0500

    545pm 4-16

--- SQLETL.sql
+++ SQLETL.sql
@@ -1,12 +1,26 @@
-CREATE TABLE book_genre (
-	Name varchar
-	Author varchar
-	User Rating varchar	
-	Reviews	varchar
-	Price integer
-	Year date	
-	Gen_Genre varchar	
-	Unnamed: 0 integer	
+DROP TABLE genre;
+CREATE TABLE genre (
+	Multiples varchar,
+	Name varchar,
 	Spec_Genre varchar
 );
+DROP TABLE books;
+CREATE TABLE books (
+	Name varchar,
+	Author varchar,
+	User_Rating varchar,
+	Reviews integer,
+	Price integer,
+	Year integer,
+	Gen_Genre varchar
+	);
 
+SELECT * FROM books;
+SELECT * FROM genre;
+
+SELECT genre.name, genre.spec_genre, books.name, books.author, books.year
+FROM genre
+INNER JOIN books on
+genre.name=books.name
+WHERE spec_genre = 'Race' OR
+	spec_genre = 'Social Justice'
