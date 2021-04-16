c4534c3ccb731ced526507ddfa3b2936cc3aca21
Brandy Knust <brandy_1175@hotmail.com>
2021-04-16 17:17:48 -0500

    515pm 4-16

---
+++ SQLETL.sql
@@ -0,0 +1,12 @@
+CREATE TABLE book_genre (
+	Name varchar
+	Author varchar
+	User Rating varchar	
+	Reviews	varchar
+	Price integer
+	Year date	
+	Gen_Genre varchar	
+	Unnamed: 0 integer	
+	Spec_Genre varchar
+);
+
